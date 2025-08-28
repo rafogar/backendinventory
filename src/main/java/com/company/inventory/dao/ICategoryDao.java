5a33ef0ad37ca5282c1fe7c864769f2ff14c8fcd
Rafael Olivares <rolivare@uninet.com>
2025-08-28 15:08:40 -0600

    servicio de obtener categorias

---
+++ src/main/java/com/company/inventory/dao/ICategoryDao.java
@@ -0,0 +1,10 @@
+package com.company.inventory.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.company.inventory.model.Category;
+
+public interface ICategoryDao extends CrudRepository<Category, Long>{
+
+	
+}
