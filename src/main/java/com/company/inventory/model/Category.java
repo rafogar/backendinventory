d0f16c3d2b71173fc1d1471681a8508f33cf0f12
Rafael Olivares <rolivare@uninet.com>
2025-08-28 20:31:33 -0600

    Servicio guardar categoria

--- src/main/java/com/company/inventory/model/Category.java
+++ src/main/java/com/company/inventory/model/Category.java
@@ -22,5 +22,7 @@ public class Category implements Serializable{
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	private String name;
+	private String description;
 
 }
