61c29a4f23f6ba39ef7d95837bbbfd9808e1dd22
Rafael Olivares <rolivare@uninet.com>
2025-08-29 11:59:07 -0600

    servicio eliminar por id

--- src/main/java/com/company/inventory/services/CategoryServiceImpl.java
+++ src/main/java/com/company/inventory/services/CategoryServiceImpl.java
@@ -128,5 +128,23 @@ public class CategoryServiceImpl implements ICategoryService {
 		}
 		return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.OK);
 	}
+	
+	@Override
+	@Transactional
+	public ResponseEntity<CategoryResponseRest> deleteById(Long id) {
+		
+		CategoryResponseRest response = new CategoryResponseRest();		
+		
+		try {
+			categoryDao.deleteById(id);
+			response.setMetadata("respuesta ok", "00", "Registro eliminado");
+			
+		} catch (Exception e) {
+			response.setMetadata("Respuesta ok",  "-1", "Error al eliminar");
+			e.getStackTrace();
+			return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.OK);
+	}
 
 }
